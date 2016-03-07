eef43ff1f46426493a42a8c721771ba9d0699723
nataliecr <pikayoshi21@gmail.com>
2016-03-07 14:41:38 -0800

    displays the pi value for i = 10000, 20000, ... , 100000.

--- src/ComputePi.java
+++ src/ComputePi.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class ComputePi {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter an integer value for i (10000 - 100000): ");
-    double value = input.nextDouble();
-    double pi = 0;
+    //System.out.print("Enter an integer value for i (10000 - 100000): ");
+    //double value = input.nextDouble();
     
-    for(double i = 1; i <= value; i++)
-      pi += Math.pow(-1, i+1) / (2*i - 1);
-    
-    System.out.println("Pi is " + 4*pi);
+    for(int j = 10000; j <= 100000; j = j + 10000){
+      double pi = 0;  
+      for(double i = 1; i <= j; i++){
+        pi += (Math.pow(-1, i+1) / (2*i - 1)) * 4;
+      }  
+        
+      System.out.println("Pi at " + j + " is " + pi);
+    } // for j 
   }  // main
     
 } // ComputePi
